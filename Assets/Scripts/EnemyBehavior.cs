144c2dc28b4de7dc7b24fc22ebc1e45b2a0d2436
ben-baqa <56326234+ben-baqa@users.noreply.github.com>
2021-01-17 20:42:59 -0700

    Updated enemy sprites

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -10,19 +10,43 @@ public class EnemyBehavior : MonoBehaviour
     public float attackDistance;
 
     private Animator anim;
+    private SpriteRenderer rend;
     private Transform target;
+
+    private bool hasTurnAnimation = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+        foreach (AnimatorControllerParameter p in anim.parameters)
+            if (p.name == "turn")
+                hasTurnAnimation = true;
         AquireTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!target)
+            return;
+
         if (Vector2.Distance(transform.position, target.position) < attackDistance)
             anim.SetTrigger("attack");
+
+        if (hasTurnAnimation)
+        {
+            if (rend.flipX && target.transform.position.x < transform.position.x)
+            {
+                rend.flipX = false;
+                anim.SetTrigger("turn");
+            }
+            else if (!rend.flipX && target.transform.position.x > transform.position.x)
+            {
+                rend.flipX = true;
+                anim.SetTrigger("turn");
+            }
+        }
     }
 
     private void AquireTarget()

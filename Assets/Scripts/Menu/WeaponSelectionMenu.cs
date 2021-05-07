9d1443e40b62b5a40f036f2d99cb08bf0034580c
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-05-07 15:40:48 -0600

    Basic weapon selection system prototype

--- Assets/Scripts/Menu/WeaponSelectionMenu.cs
+++ Assets/Scripts/Menu/WeaponSelectionMenu.cs
@@ -1,59 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponSelectionMenu : MonoBehaviour
 {
     public GameObject canvas;
-    public SpriteRenderer back, icon;
+    public SpriteRenderer back, icon, prevIcon;
+    public List<SelectionItem> options;
+    public Text desc, level, overallLevel;
+
+    public float lerpStart, lerpMod;
 
     private Animator anim;
     private GameController gcon;
-    private ControlKey pKey;
     private CameraFollow camFollow;
- 
-    private bool interactable = false, open = false, input = false, inputToggle = true;
+
+    private SelectionItem selected;
+    private Vector2 lerpGoal;
+    private ControlToggleProxy activate, left, right;
+
+    private float lerpVal = 0;
+    private bool interactable = false, open = false;
     // Start is called before the first frame update
     void Start()
     {
         //canvas = GetComponentInChildren<Canvas>().gameObject;
         anim = GetComponent<Animator>();
         gcon = GameObject.FindObjectOfType<GameController>();
-        pKey = GameObject.FindGameObjectWithTag("pControl").GetComponent<ControlKey>();
         camFollow = Camera.main.GetComponent<CameraFollow>();
         canvas.SetActive(false);
+
+        activate = new ControlToggleProxy();
+        left = new ControlToggleProxy();
+        right = new ControlToggleProxy();
+        ControlKey pKey = GameObject.FindGameObjectWithTag("pControl").GetComponent<ControlKey>();
+        activate.Setup(pKey, "down");
+        left.Setup(pKey, "left");
+        right.Setup(pKey, "right");
+
+        SelectCurrentWeapon();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inputToggle)
-            inputToggle = pKey["down"];
-        else
-            input = pKey["down"];
+        activate.Update();
+        left.Update();
+        right.Update();
 
-        if (interactable && input)
+        if (interactable && activate.val)
             ToggleCabinet();
 
+        if (open)
+        {
+            if (left.val)
+                SwitchSelection(1);
+            else if (right.val)
+                SwitchSelection(-1);
+        }
+
+        RunSwap();
     }
 
-    public void ChangeWeapon(GameObject weapPrefab)
+    private void SelectCurrentWeapon()
     {
-        Debug.Log(weapPrefab);
-        canvas.SetActive(false);
-        camFollow.ResetFollowOverride();
-        //pKey.enabled = true;
-        //Time.timeScale = 1;
-        gcon.SetCurrentWeap(weapPrefab);
-        //Destroy(GameObject.FindGameObjectWithTag("Player"));
-        Instantiate(weapPrefab, transform.position, Quaternion.identity);
+        selected = options[0];
+        foreach (SelectionItem i in options)
+            if (i.prefab == gcon.currentWeaponPrefab)
+                selected = i;
+        DisplaySelectedAttributes();
     }
 
     private void ToggleCabinet()
     {
-        input = false;
-        inputToggle = true;
         open = !open;
+        anim.SetBool("open", open);
+        canvas.SetActive(open);
         if (open)
         {
             Destroy(GameObject.FindGameObjectWithTag("Player"));
@@ -65,8 +88,49 @@ public class WeaponSelectionMenu : MonoBehaviour
             Instantiate(gcon.currentWeaponPrefab,
                 (Vector2)transform.position + Vector2.down * .8f, Quaternion.identity);
         }
-        anim.SetBool("open", open);
-        canvas.SetActive(open);
+    }
+
+    private void SwitchSelection(int i)
+    {
+        prevIcon.enabled = true;
+        prevIcon.transform.localPosition = Vector2.zero;
+        prevIcon.sprite = selected.sprite;
+        lerpGoal = new Vector2(-i, 0) * .75f;
+        lerpVal = lerpStart;
+
+        int newIndex = (options.IndexOf(selected) + i) % options.Count;
+        if (newIndex < 0)
+            newIndex = options.Count + newIndex;
+        selected = options[newIndex];
+
+        icon.transform.localPosition = new Vector2(i, 0) * .75f;
+        DisplaySelectedAttributes();
+
+        gcon.SetCurrentWeap(selected.prefab);
+    }
+
+    private void DisplaySelectedAttributes()
+    {
+        icon.sprite = selected.sprite;
+        desc.text = selected.description;
+    }
+
+    private void RunSwap()
+    {
+        if (open && prevIcon.enabled)
+        {
+            prevIcon.transform.localPosition =
+                Vector2.Lerp(prevIcon.transform.localPosition, lerpGoal, lerpVal);
+            icon.transform.localPosition =
+                Vector2.Lerp(icon.transform.localPosition, Vector2.zero, lerpVal);
+            lerpVal *= lerpMod;
+
+            if (Vector2.Distance(icon.transform.localPosition, Vector2.zero) < 0.01f)
+            {
+                prevIcon.enabled = false;
+                icon.transform.localPosition = Vector2.zero;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -83,13 +147,17 @@ public class WeaponSelectionMenu : MonoBehaviour
         interactable = false;
     }
 
-    private void OnDoorOpen()
-    {
-        back.enabled = icon.enabled = true;
-    }
+    private void OnDoorOpen() { back.enabled = icon.enabled = true; }
 
-    private void OnDoorClose()
-    {
-        back.enabled = icon.enabled = false;
-    }
+    private void OnDoorClose() { back.enabled = icon.enabled = prevIcon.enabled = false; }
+}
+
+[System.Serializable]
+public class SelectionItem
+{
+    public string name, description;
+    public GameObject prefab;
+    public Sprite sprite;
+
+    public int[] stats = new int[3]; //speed, power, usability
 }
---
+++ Assets/Scripts/Player/ControlToggleProxy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlToggleProxy
+{
+    public bool val
+    {
+        get
+        {
+            if (value)
+            {
+                value = false;
+                toggle = true;
+                return true;
+            }
+            return false;
+        }
+    }
+    private bool toggle = true, value = false;
+
+    private ControlKey key;
+    private string control;
+
+    public void Setup(ControlKey k, string s)
+    {
+        key = k;
+        control = s;
+    }
+
+    public void Update()
+    {
+        if (toggle)
+            toggle = key[control];
+        else
+            value = key[control];
+    }
+}

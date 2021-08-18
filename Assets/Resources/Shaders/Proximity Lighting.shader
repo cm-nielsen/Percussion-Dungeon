e737dad70e4fa776d9e36d9fcaf6f070f1e38387
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-08-17 18:33:29 -0600

    Fixed proximity lighting system

--- Assets/Resources/Shaders/Proximity Lighting.shader
+++ Assets/Resources/Shaders/Proximity Lighting.shader
@@ -1,4 +1,4 @@
-﻿Shader "Custom/PsuedoLighting"
+﻿Shader "Custom/Proximity Lighting"
 {
     Properties
     {
@@ -13,7 +13,8 @@
 
         Pass
         {
-        Blend SrcAlpha OneMinusSrcAlpha
+            Name "Pass 1"
+            Blend SrcAlpha OneMinusSrcAlpha
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
@@ -35,8 +36,8 @@
                 float2 worldPos : TEXCOORD2;
             };
 
-            float _PositionData[10000];
-            int _PosArrayWidth;
+            float4 _PositionData[1024];
+            int _PosCount;
             float _BaseLight;
 
             sampler2D _MainTex;
@@ -57,7 +58,7 @@
             fixed4 frag (v2f i) : SV_Target
             {
                 fixed4 col = tex2D(_MainTex, i.uv);
-                
+
                 //Apply Colour Customization effect
                 float n = step(col.r + col.g + col.b, .1);
 
@@ -69,20 +70,20 @@
 
                 col *= i.color;
 
-                //Apply PsuedoLighting
+                //Apply Proximity Lighting
                 float2 wPos = i.worldPos.xy;
-                float light = _PositionData[wPos.x * _PosArrayWidth + wPos.y];
+                float light = 0;
+                
                 float3 p;
+                for (int j = 0; j < _PosCount; j++) {
+                    p = _PositionData[j];
+                    //float dist = distance(p.xy, i.worldPos.xy);
+                    float dist = pow(p.x - wPos.x, 2) + pow(p.y - wPos.y, 2);
 
-                //for (int j = 0; j < _PosCount; j++) {
-                //    p = _PositionData[j];
-                //    //float dist = distance(p.xy, i.worldPos.xy);
-                //    float dist = pow(p.x - wPos.x, 2) + pow(p.y - wPos.y, 2);
+                    light += p.z / dist;
+                }
 
-                //    light += p.z / dist;
-                //}
-
-                //col.xyz *=  clamp(_BaseLight + light, 0, 1);
+                col.xyz *=  clamp(_BaseLight + light, 0, 1);
                 return col;
             }
             ENDCG
---
+++ Assets/Scripts/Camera + vfx/ProximityLightingSystem.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityLightingSystem : MonoBehaviour
+{
+    public MaterialLightingSettings[] members;
+
+    private const int maxPoints = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Vector4> data = new List<Vector4>();
+        PlayerController p = FindObjectOfType<PlayerController>();
+        Vector3 v = Vector3.zero;
+        if (p)
+        {
+            v = p.transform.position;
+            v.z = -1;
+            data.Add(v);
+        }
+        foreach(EnemyBehavior e in FindObjectsOfType<EnemyBehavior>())
+        {
+            v = e.transform.position;
+            v.z = 0;
+            data.Add(v);
+            if (data.Count == maxPoints)
+            {
+                print("too many enemies for lighting system to handle");
+                break;
+            }
+        }
+
+        foreach (MaterialLightingSettings m in members)
+            m.ApplyData(data, maxPoints);
+    }
+
+    [System.Serializable]
+    public class MaterialLightingSettings
+    {
+        public Material mat, colourReference;
+        public float playerRadius, enemyRadius, baseLight,
+            playerStrength, enemyStrength;
+
+        public void ApplyData(List<Vector4> a, int maxPoints)
+        {
+            List<Vector4> data = new List<Vector4>();
+            float pr = Mathf.Pow(playerRadius, 2),
+                er = Mathf.Pow(enemyRadius, 2);
+
+            foreach(Vector4 v in a)
+            {
+                if (v.z == -1)
+                    data.Add(new Vector4(v.x, v.y, pr, playerStrength));
+                else
+                    data.Add(new Vector4(v.x, v.y, er, enemyStrength));
+            }
+            while (data.Count < maxPoints)
+                data.Add(Vector4.zero);
+
+            mat.SetVectorArray("_PositionData", data.ToArray());
+            mat.SetInt("_PosCount", a.Count);
+            mat.SetFloat("_BaseLight", baseLight);
+
+            mat.SetColor("_MainCol", colourReference.GetColor("_MainCol"));
+            mat.SetColor("_MonoCol", colourReference.GetColor("_MonoCol"));
+        }
+    }
+}
